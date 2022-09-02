48def70125c9c7c230665ffe3d170bdbc9d5e378
Nnanna Jude <judegr8@yahoo.com>
2022-09-02 13:42:22 +0100

    add accordion toggle

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('hello')
+//variables
+
+const answer = document.getElementsByClassName('answer');
+const accordion = document.getElementsByClassName('content-container');
+
+for (let i = 0; i < accordion.length; i++) {
+    
+    accordion[i].addEventListener('click', function(){
+        this.classList.toggle('active');
+
+} )
+}
